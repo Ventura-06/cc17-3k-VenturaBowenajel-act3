10c6c0219508ecfd609be33023023f6c48f68e35
Bowen Ajel Ventura <iamventura6@gmail.com>
2024-10-03 17:15:02 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/dicerollerapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.dicerollerapp
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        val diceImage: ImageView = findViewById(R.id.dice_image)
+
+        rollButton.setOnClickListener {
+            rollDice(diceImage)
+        }
+    }
+
+    private fun rollDice(diceImage: ImageView) {
+        val diceRoll = (1..6).random()
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+    }
+}
